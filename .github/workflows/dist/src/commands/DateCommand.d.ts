ed6e35686c2dfea935cb98f88be8777a72d61146
uthnart <tranthu333@gmail.com>
2023-06-15 16:02:10 -0400

    Delete .github/workflows/dist directory

--- .github/workflows/dist/src/commands/DateCommand.d.ts
+++
@@ -1,7 +0,0 @@
-import { Command } from 'commander';
-import { GenericCommand } from './GenericCommand.js';
-/** Command to print out current date in various formats */
-export declare class DateCommand extends GenericCommand {
-    constructor(name: string, program: Command);
-    run(options: any): Promise<void>;
-}
